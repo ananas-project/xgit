51dd093b7e3368458946c71d56370ed56c7cd354
xukun <xukun0217@gmail.com>
2013-08-05 00:01:40 +0800

    update : to bp4 & axk2.

--- xgit-core/src/ananas/lib/xgit/XGitRepoFactory.java
+++ xgit-core/src/ananas/lib/xgit/XGitRepoFactory.java
@@ -1,9 +1,26 @@
 package ananas.lib.xgit;
 
 import ananas.lib.io.vfs.VFile;
+import ananas.lib.util.SingletonLoader;
 
 public interface XGitRepoFactory {
 
 	XGitRepo createRepo(VFile file);
+
+	class Util {
+
+		private static XGitRepoFactory inst;
+
+		public static XGitRepoFactory getDefaultFactory() {
+			XGitRepoFactory factory = inst;
+			if (factory == null) {
+				factory = (XGitRepoFactory) SingletonLoader
+						.load(XGitRepoFactory.class);
+				inst = factory;
+			}
+			return factory;
+		}
+
+	}
 
 }
